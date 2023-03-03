c5b4507b15513b0af0193c802c0bb81c53635932
acgecloud <acgecloudtrader@gmail.com>
2023-03-03 04:18:39 +0000

    Created multi-stage build, still resolving compose

--- backend-flask/external-script-cmd-backend.sh
+++ backend-flask/external-script-cmd-backend.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # runs the starting backend task
-docker run --rm -p 4567:4567 -it -e FRONTEND_URL='*' -e BACKEND_URL='*' backend-flask:V1 python3 -m flask run --host=0.0.0.0 --port=4567
+# Enter the appropriate tag name for running this script
+docker run --rm -p 4567:4567 -it -e FRONTEND_URL='*' -e BACKEND_URL='*' backend-flask:$1 python3 -m flask run --host=0.0.0.0 --port=4567
--- frontend-react-js/external-script-cmd-frontend.sh
+++ frontend-react-js/external-script-cmd-frontend.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # runs the starting frontend task
-docker run --rm -it -p 3000:3000 -d frontend-react-js:V1 npm start
+# Enter the appropriate tag name for running this script
+docker run --rm -it -p 3000:3000 -d frontend-react-js:$1 npm start
+# Latest multi-build stage docker command:
+# docker run --rm -it -p 80:80 -d frontend-react-js:$1 nginx -g daemon off;
