900e90aaa48be4dc3d585eb330e5268fd1706508
acgecloud <acgecloudtrader@gmail.com>
2023-03-04 01:52:26 +0000

    Updated front-end with multi-stage & health check

--- frontend-react-js/external-script-cmd-frontend.sh
+++ frontend-react-js/external-script-cmd-frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # runs the starting frontend task
 # Enter the appropriate tag name for running this script
-docker run --rm -it -p 3000:3000 -d frontend-react-js:$1 npm start
+docker run -p 3000:3000 -d frontend-react-js:$1 npm start
 # Latest multi-build stage docker command:
 # docker run --rm -it -p 80:80 -d frontend-react-js:$1 nginx -g daemon off;
